af0bf65dcdde5dbe77adc038c5349c242f1cae2f
EdnaTMunill <80659091+Ethra8@users.noreply.github.com>
2026-07-07 23:56:56 +0200

    unify css class

--- profiles/static/profiles/js/company_admin.js
+++ profiles/static/profiles/js/company_admin.js
@@ -1,3 +1,22 @@
+// GO TO PREVIOUS PAGE -- GO BACK
+document.addEventListener("DOMContentLoaded", function () {
+    const backLinks = document.querySelectorAll(".js-back-link");
+
+    backLinks.forEach(function (link) {
+        link.addEventListener("click", function (event) {
+            event.preventDefault();
+
+            if (window.history.length > 1) {
+                window.history.back();
+            } else {
+                window.location.href = "/";
+            }
+        });
+    });
+});
+
+
+
 // COMPANY ADMIN COURSE ATTENDANCE FILTERS
 // for company_admin_course_attendance.html
 document.addEventListener("DOMContentLoaded", function () {
