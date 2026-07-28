ca405fd8283c93c66f7aec3be1e2ee66249005c1
EdnaTMunill <80659091+Ethra8@users.noreply.github.com>
2026-07-28 17:25:19 +0200

    update s3 bucket settings

--- config/settings.py
+++ config/settings.py
@@ -220,13 +220,9 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 
-AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
-AWS_S3_REGION_NAME = os.environ.get(
-    "AWS_S3_REGION_NAME",
-    "eu-north-1",
-)
+AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME")
+
 
 AWS_DEFAULT_ACL = None
 AWS_S3_FILE_OVERWRITE = False
@@ -235,8 +231,6 @@ STORAGES = {
     "default": {
         "BACKEND": "storages.backends.s3.S3Storage",
         "OPTIONS": {
-            "access_key": AWS_ACCESS_KEY_ID,
-            "secret_key": AWS_SECRET_ACCESS_KEY,
             "bucket_name": AWS_STORAGE_BUCKET_NAME,
             "region_name": AWS_S3_REGION_NAME,
             "default_acl": None,
