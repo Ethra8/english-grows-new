7483133f62c23c977680a7b31fbae8ef9f2d650c
EdnaTMunill <80659091+Ethra8@users.noreply.github.com>
2026-09-24 04:48:15 +0200

    Allow server to display django errors in server logs

--- config/settings.py
+++ config/settings.py
@@ -314,3 +314,21 @@ STORAGES = {
         ),
     },
 }
+
+# Django send server Render errors raised to reproduce in logs
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "django.request": {
+            "handlers": ["console"],
+            "level": "ERROR",
+            "propagate": False,
+        },
+    },
+}
